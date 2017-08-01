49cda891d686e5fe71a07a4ac20ba02879e49c6f
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-08-01 11:13:58 +0600

    Changed webpack.config.js and updated react to 15.6.1

--- webpack.config.js
+++ webpack.config.js
@@ -50,9 +50,18 @@ module.exports = {
                 loader: "json-loader",
                 exclude: [/.babelrc/]
             },
-                { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader:"url?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
         ]
     }
 }
