55efacecc15326e819cf7c17e686b450a43b9061
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-08-04 17:23:25 +0600

    Changed store

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -7,17 +7,8 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import todoApp from '../reducers/index';
 
-export default function configureStore(initialState) {
-    const middewares = [
-        // Add other middleware on this line...
-
-        // thunk middleware can also accept an extra argument to be passed to each thunk action
-        // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-        thunk,
-    ];
-
-    return createStore(todoApp, initialState, compose(
-        applyMiddleware(...middewares)
-        )
+export default function configureStore() {
+    return createStore(
+        todoApp,  applyMiddleware(thunk)
     );
 }
